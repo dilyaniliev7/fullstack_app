8ffe1bd9d1fc742727a1c0ec06d8b3dd8917fd0b
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-01-27 19:26:56 +0200

    frontend setup created

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -20,17 +20,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://localhost:5173',
 ]
 
 ROOT_URLCONF = 'crud.urls'
