1c5ead5d058da10492af3b7c8f615995a8e7b399
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-01-30 21:14:56 +0200

    backend API created with Django Rest Framework

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.contrib import admin
+from django.urls import path
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('country', CountryViewSet, basename='country')
+router.register('league', LeagueViewSet, basename='league')
+router.register('characteristic', CharacteristicViewSet, basename='characteristic')
+
+urlpatterns = router.urls
--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
     'corsheaders',
     'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
