0d7a63499c3e801456234e9c8329b658fee86d7f
dilyaniliev7 <dilyaniliev7@abv.bg>
2026-01-31 12:43:02 +0200

    get django API data in react using axios

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import AxiosInstance from "./Axios"
 
 const Create = () => {
+    const [country, setCountry] = useState([])
+    const [league, setLeague] = useState([])
+    const [characteristic, setCharacteristic] = useState([])
+    const GetData = () => {
+        AxiosInstance.get(`country/`).then((res) => {
+            setCountry(res.data)
+            })
+        AxiosInstance.get(`league/`).then((res) => {
+            setLeague(res.data)
+            })
+        AxiosInstance.get(`characteristic/`).then((res) => {
+            setCharacteristic(res.data)
+            })
+        }
+    useEffect(() => {
+        GetData()
+        }, [])
     return (
         <div>
             This is the create page
